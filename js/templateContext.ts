4c3b84966b8d3142f324fca989b2887bebdfd8ea
Matthew Wells <matthewpaulwells@gmail.com>
2017-01-08 14:59:22 -0800

    Switch template context to arrow functions. Very terse.

--- js/templateContext.ts
+++ js/templateContext.ts
@@ -1,17 +1,5 @@
 export default {
-    __: function () {
-        return function (text) {
-            return text;
-        }
-    },
-    quoteEscape: function () {
-        return function (text) {
-            return text;
-        }
-    },
-    stripTags: function () {
-        return function (text) {
-            return text;
-        }
-    }
+    __: () => text => text,
+    quoteEscape: () => text => text,
+    stripTags: () => text => text
 };
